1b6f50e7f826944f34f1f1e04dd972e158553bc7
CAZORBONNETAdrian <168072200+CAZORBONNETAdrian@users.noreply.github.com>
2024-10-21 18:50:40 +0200

    Modification de la classe visite

--- gestion_donnees/Visite.java
+++ gestion_donnees/Visite.java
@@ -12,11 +12,11 @@ public class Visite {
 	private Date dateVisite;
 	private Date heureVisite;
 	private String telephone;
-	private String exposition;
-	private String employe;
-	private String conferencier;
-	
-	public Visite(String idVisite, String expositionId, String conferencierId, String employeId, String dateVisite, String heureVisite, String intitule, String numTel) throws VisiteException, EmployeException {
+	private String expositionId;
+	private String employeId;
+	private String conferencierId;
+	// String expositionId, String conferencierId, String employeId, 
+	public Visite(String idVisite, String dateVisite, String heureVisite, String intitule, String numTel) throws VisiteException, EmployeException {
 		
 		SimpleDateFormat jourVisite = new SimpleDateFormat("dd/MM/yyyy");
 		SimpleDateFormat heureDebut = new SimpleDateFormat("HH'h'mm");
@@ -26,71 +26,8 @@ public class Visite {
 		
 		if (idVisite.length() != 7 || idVisite == null) {
 			throw new VisiteException();
-		} else {
-			for (Visite visite : DonneesApplication.visites) {
-				if (visite.getId() == id) {
-					throw new VisiteException();
-				}
-			}
-			this.id = idVisite;
 		}
-		
-		
-		if (employeId.length() != 7 || employeId == null) {
-			throw new VisiteException();
-		} else {
-			boolean employeTrouvé = false;
-			for (Employe employe : DonneesApplication.employes) {
-				if (employe.getId() == employeId) {
-					employeTrouvé = true;
-				}
-			}
-			if (employeTrouvé) {
-			     this.employe = employeId;
-			 } else {
-			     throw new VisiteException();
-			 }
-		}
-		
-		
-		
-		if (expositionId.length() != 7 || expositionId == null) {
-			throw new VisiteException();
-		} else {
-			boolean expositionTrouvée = false;
 			
-			for (Exposition exposition : DonneesApplication.expositions) {
-			        if (exposition.getId() == expositionId) {
-			            expositionTrouvée = true;
-			        }
-			    }
-			    
-			 if (expositionTrouvée) {
-			     this.exposition = employeId;
-			 } else {
-			     throw new VisiteException();
-			 }
-		}
-		
-		if (conferencierId.length() != 7 || conferencierId == null) {
-			throw new VisiteException();
-		} else {
-			boolean conferencierTrouvé = false;
-			
-			for (Conferencier conferenciers : DonneesApplication.conferenciers) {
-			        if (conferenciers.getId() == conferencierId) {
-			            conferencierTrouvé = true;
-			        }
-			    }
-			    
-			 if (conferencierTrouvé) {
-				 this.conferencier = conferencierId;
-			 } else {
-			     throw new VisiteException();
-			 }
-		}
-		
-		
 		if (numTel.length() != 9 || numTel == null) {
 			throw new VisiteException();
 		}
@@ -109,10 +46,10 @@ public class Visite {
 		} else {
 			throw new VisiteException();
 		}
-			
+		
+		this.id = idVisite;
 		this.intitule = intitule;
 		this.telephone = numTel;
-		DonneesApplication.visites.add(this);
 	}
 	
 	public String getId() {
@@ -123,38 +60,25 @@ public class Visite {
 		return "	Visite : " + this.intitule;
 	}
 	
-	public static void main(String args[]) throws VisiteException, EmployeException, ExpositionException, ConferencierException {
-
-	    DonneesApplication.initialisesDonnees();
-	    
-	    String[] tab = new String[10];
-		tab[0] = "dbzayudza";
-		tab[1] = "salut";
-		
-		String[] specialites = {"Java", "AI", "Data Science"};
-		
-		ArrayList<Date> indisponibilite = new ArrayList<>();
-        indisponibilite.add(new Date()); 
-	    
-
-	    Employe employeLiée = new Employe("1111111", "Clement", "juery", "1234");
-	    
-
-	    Exposition expositionLiée = new Exposition("E000002", "salut", "1984", "1987", 6, tab, "dzayudfgauyfaf", "12/10/2012", "12/12/2024");
-	    
-	    		
-	    Conferencier conferencier = new Conferencier("ABC1234","Dupont","Jean",true,indisponibilite, specialites,"0612345678");
-
-	    String idVisite = "VST1234"; 
-	    String intitule = "Visite guidée des chefs-d'œuvre";
-	    String dateVisite = "18/12/2020";  
-	    String numTel = "111111111";
-	    String heureVisite = "15h00";
-	    
-	    Visite visite1 = new Visite(idVisite, "E000002", "ABC1234", "1111111", dateVisite, heureVisite, intitule, numTel);
-	        
-	    System.out.println(visite1.toString());
-	  
+	public void setExpositionId(String expositionId) throws VisiteException {
+		if (expositionId.length() != 7 || expositionId == null) {
+			throw new VisiteException();
+		}
+		this.expositionId = expositionId;
 	}
-
+	
+	public void setConferencierId(String conferencierId) throws VisiteException {
+		if (conferencierId.length() != 7 || conferencierId == null) {
+			throw new VisiteException();
+		}
+			this.conferencierId = conferencierId;
+	}
+	
+	public void setEmployeId(String employeId) throws VisiteException {
+		if (employeId.length() != 7 || employeId == null) {
+			throw new VisiteException();
+		} else {
+			this.employeId = employeId;
+		}
+	}
 }
