1c7f1540f44e6cc0ceefce981e92ae584865c724
qchesnier <168072017+qchesnier@users.noreply.github.com>
2024-10-24 15:06:51 +0200

    CA FONCTIONNE mais changement à faire

--- reseau/client.java
+++ reseau/client.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
 	private static DataOutputStream dataOutputStream = null;
@@ -12,6 +13,20 @@ public class client {
 			Socket socket = new Socket("localhost",5004);
 			dataInputStream = new DataInputStream(socket.getInputStream());
 			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			Scanner entree = new Scanner(System.in);
+			String requete;
+
+			// get the output stream from the socket.
+			OutputStream outputStream = socket.getOutputStream();
+			// create a data output stream from the output stream so we can send data through it
+			DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+			
+			System.out.print("Entre le fichier que vous souhaitez : ");
+			requete = entree.nextLine();
+
+			// write the message we want to send
+			dataOutputStream.writeUTF(requete);
+			dataOutputStream.flush(); // send the message
 
 			receiveFile("NewFile1.csv");
 
@@ -21,7 +36,7 @@ public class client {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void receiveFile(String fileName) throws Exception{
 		int bytes = 0;
 		FileOutputStream fileOutputStream = new FileOutputStream(fileName);
--- reseau/server.java
+++ reseau/server.java
@@ -5,50 +5,58 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
-    private static DataOutputStream dataOutputStream = null;
-    private static DataInputStream dataInputStream = null;
+	private static DataOutputStream dataOutputStream = null;
+	private static DataInputStream dataInputStream = null;
 
-    public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(5004)){
-            System.out.println("listening to port:5000");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket+" connected.");
-            dataInputStream = new DataInputStream(clientSocket.getInputStream());
-            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-            
-            switch (requete) {
-            
-	            case "employe" : 
-	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\employe.csv");
-	            	break;
-	            case "conferencier" :
-	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\conferencier.csv");
-	            	break;
-	            case "exposition" : 
-	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\exposition.csv");
-	            	break;
-	            case "visite" :
-	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\visite.csv");
-	            	break;
-	            default : 
-	            	// TODO gérer l'erreur
-            }
-            	
-            
+	public static void main(String[] args) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(5004);
+			System.out.println("listening to port:5000");
+			Socket clientSocket = serverSocket.accept();
+			System.out.println(clientSocket+" connected.");
+			dataInputStream = new DataInputStream(clientSocket.getInputStream());
+			dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+			
+			// get the input stream from the connected socket
+			InputStream inputStream = clientSocket.getInputStream();
+			
+			DataInputStream dataInputStream = new DataInputStream(inputStream);
 
-            dataInputStream.close();
-            dataOutputStream.close();
-            clientSocket.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+			// read the message from the socket
+			String requete = dataInputStream.readUTF();
 
-    private static void sendFile(String path) throws Exception{
+			switch (requete) {
+				case "employe" : 
+					sendFile("Z:\\Projet\\workspace\\TestReseau2\\employe.csv");
+					break;
+				case "conferencier" :
+					sendFile("Z:\\Projet\\workspace\\TestReseau2\\conferencier.csv");
+					break;
+				case "exposition" : 
+					sendFile("Z:\\Projet\\workspace\\TestReseau2\\exposition.csv");
+					break;
+				case "visite" :
+					sendFile("Z:\\Projet\\workspace\\TestReseau2\\visite.csv");
+					break;
+				default : 
+					// TODO gérer l'erreur
+			}
+
+			dataInputStream.close();
+			dataOutputStream.close();
+			clientSocket.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	private static void sendFile(String path) throws Exception{
 		int bytes = 0;
 		File file = new File(path);
 		FileInputStream fileInputStream = new FileInputStream(file);
