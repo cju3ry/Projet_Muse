3de03e5a099940a44a6b85e57bd4ecb7f1fba898
qchesnier <168072017+qchesnier@users.noreply.github.com>
2024-10-24 16:24:53 +0200

    nouvelle version server et client de test

--- reseau/client.java
+++ reseau/client.java
@@ -1,52 +1,40 @@
 package test;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
 import java.util.Scanner;
 
 public class client {
-	private static DataOutputStream dataOutputStream = null;
-	private static DataInputStream dataInputStream = null;
-
 	public static void main(String[] args) {
-		try {
-			Socket socket = new Socket("localhost",5004);
-			dataInputStream = new DataInputStream(socket.getInputStream());
-			dataOutputStream = new DataOutputStream(socket.getOutputStream());
-			Scanner entree = new Scanner(System.in);
-			String requete;
+		String serverAddress = "10.2.19.59"; // Adresse IP du serveur
+		int port = 12345; // Port du serveur
 
-			// get the output stream from the socket.
-			OutputStream outputStream = socket.getOutputStream();
-			// create a data output stream from the output stream so we can send data through it
-			DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+		try {
+			Socket socket = new Socket(serverAddress, port);
+			InputStream input = socket.getInputStream();
+	        BufferedInputStream bufferedInput = new BufferedInputStream(input);
+	        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+	        FileOutputStream fileOutput = new FileOutputStream("received_file.csv");
+	        
+	        Scanner entree = new Scanner(System.in);
+			String requete;  
 			
 			System.out.print("Entre le fichier que vous souhaitez : ");
 			requete = entree.nextLine();
+			
+			writer.println(requete);
+	        
+			byte[] buffer = new byte[4096];
+			int bytesRead;
 
-			// write the message we want to send
-			dataOutputStream.writeUTF(requete);
-			dataOutputStream.flush(); // send the message
-
-			receiveFile("NewFile1.csv");
-
-			dataInputStream.close();
-			dataInputStream.close();
-		}catch (Exception e){
+			while ((bytesRead = bufferedInput.read(buffer)) != -1) {
+                fileOutput.write(buffer, 0, bytesRead);
+            }
+			
+			System.out.println("Fichier reçu avec succès !");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	private static void receiveFile(String fileName) throws Exception{
-		int bytes = 0;
-		FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-
-		long size = dataInputStream.readLong();     // read file size
-		byte[] buffer = new byte[4*1024];
-		while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
-			fileOutputStream.write(buffer,0,bytes);
-			size -= bytes;      // read upto file size
-		}
-		fileOutputStream.close();
-	}
 }
+
--- reseau/server.java
+++ reseau/server.java
@@ -1,74 +1,49 @@
 package test;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class server {
-	private static DataOutputStream dataOutputStream = null;
-	private static DataInputStream dataInputStream = null;
+    public static void main(String[] args) {
+        int port = 12345; // Port d'écoute
 
-	public static void main(String[] args) {
-		try {
-			ServerSocket serverSocket = new ServerSocket(5004);
-			System.out.println("listening to port:5000");
-			Socket clientSocket = serverSocket.accept();
-			System.out.println(clientSocket+" connected.");
-			dataInputStream = new DataInputStream(clientSocket.getInputStream());
-			dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-			
-			// get the input stream from the connected socket
-			InputStream inputStream = clientSocket.getInputStream();
-			
-			DataInputStream dataInputStream = new DataInputStream(inputStream);
+        try {
+        	ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Serveur en attente de connexion...");
+            
+            try (Socket socket = serverSocket.accept()) {
+                System.out.println("Connexion établie avec " + socket.getInetAddress());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-16")); 
+                String chemin;
+                String requete = reader.readLine();
+                
+                if (requete.equals("employe")
+                	|| requete.equals("conferencier")
+                	|| requete.equals("exposition")
+                	|| requete.equals("visite")) {
+                	chemin = requete + ".csv";
+                } else {
+                	throw new IOException();
+                }
+    			
+    			// Flux d'entrée pour recevoir le fichier
+                OutputStream output = socket.getOutputStream();
+                BufferedOutputStream bufferedOutput = new BufferedOutputStream(output);
+                FileInputStream fileInput = new FileInputStream(chemin);
+                
+                byte[] buffer = new byte[4096];
+                int bytesRead;
 
-			// read the message from the socket
-			String requete = dataInputStream.readUTF();
-
-			switch (requete) {
-				case "employe" : 
-					sendFile("Z:\\Projet\\workspace\\TestReseau2\\employe.csv");
-					break;
-				case "conferencier" :
-					sendFile("Z:\\Projet\\workspace\\TestReseau2\\conferencier.csv");
-					break;
-				case "exposition" : 
-					sendFile("Z:\\Projet\\workspace\\TestReseau2\\exposition.csv");
-					break;
-				case "visite" :
-					sendFile("Z:\\Projet\\workspace\\TestReseau2\\visite.csv");
-					break;
-				default : 
-					// TODO gérer l'erreur
-			}
-
-			dataInputStream.close();
-			dataOutputStream.close();
-			clientSocket.close();
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
-
-	private static void sendFile(String path) throws Exception{
-		int bytes = 0;
-		File file = new File(path);
-		FileInputStream fileInputStream = new FileInputStream(file);
-
-		// send file size
-		dataOutputStream.writeLong(file.length());  
-		// break file into chunks
-		byte[] buffer = new byte[4*1024];
-		while ((bytes=fileInputStream.read(buffer))!=-1){
-			dataOutputStream.write(buffer,0,bytes);
-			dataOutputStream.flush();
-		}
-		fileInputStream.close();
-	}
+                // Lire le flux et écrire dans le fichier
+                while ((bytesRead = fileInput.read(buffer)) != -1) {
+                	bufferedOutput.write(buffer, 0, bytesRead);
+                }
+                
+                bufferedOutput.flush();
+                System.out.println("Fichier envoyé avec succès !");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
