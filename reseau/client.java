09a4a8dd56b277995fff9032abe6d4c5b8b2e41a
qchesnier <168072017+qchesnier@users.noreply.github.com>
2024-10-24 14:45:46 +0200

    échange envoie entre serveur et client

--- reseau/client.java
+++ reseau/client.java
@@ -4,36 +4,34 @@ import java.io.*;
 import java.net.Socket;
 
 public class client {
-    private static DataOutputStream dataOutputStream = null;
-    private static DataInputStream dataInputStream = null;
+	private static DataOutputStream dataOutputStream = null;
+	private static DataInputStream dataInputStream = null;
 
-    public static void main(String[] args) {
-        try(Socket socket = new Socket("localhost",5004)) {
-            dataInputStream = new DataInputStream(socket.getInputStream());
-            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost",5004);
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-            sendFile("Z:\\Projet\\workspace\\TestReseau2\\employe.csv");
-            
-            dataInputStream.close();
-            dataInputStream.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+			receiveFile("NewFile1.csv");
 
-    private static void sendFile(String path) throws Exception{
-        int bytes = 0;
-        File file = new File(path);
-        FileInputStream fileInputStream = new FileInputStream(file);
-        
-        // send file size
-        dataOutputStream.writeLong(file.length());  
-        // break file into chunks
-        byte[] buffer = new byte[4*1024];
-        while ((bytes=fileInputStream.read(buffer))!=-1){
-            dataOutputStream.write(buffer,0,bytes);
-            dataOutputStream.flush();
-        }
-        fileInputStream.close();
-    }
+			dataInputStream.close();
+			dataInputStream.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static void receiveFile(String fileName) throws Exception{
+		int bytes = 0;
+		FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+
+		long size = dataInputStream.readLong();     // read file size
+		byte[] buffer = new byte[4*1024];
+		while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+			fileOutputStream.write(buffer,0,bytes);
+			size -= bytes;      // read upto file size
+		}
+		fileOutputStream.close();
+	}
 }
---
+++ reseau/server.java
@@ -0,0 +1,66 @@
+package test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket = new ServerSocket(5004)){
+            System.out.println("listening to port:5000");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println(clientSocket+" connected.");
+            dataInputStream = new DataInputStream(clientSocket.getInputStream());
+            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+            
+            switch (requete) {
+            
+	            case "employe" : 
+	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\employe.csv");
+	            	break;
+	            case "conferencier" :
+	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\conferencier.csv");
+	            	break;
+	            case "exposition" : 
+	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\exposition.csv");
+	            	break;
+	            case "visite" :
+	            	sendFile("Z:\\Projet\\workspace\\TestReseau2\\visite.csv");
+	            	break;
+	            default : 
+	            	// TODO gérer l'erreur
+            }
+            	
+            
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            clientSocket.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendFile(String path) throws Exception{
+		int bytes = 0;
+		File file = new File(path);
+		FileInputStream fileInputStream = new FileInputStream(file);
+
+		// send file size
+		dataOutputStream.writeLong(file.length());  
+		// break file into chunks
+		byte[] buffer = new byte[4*1024];
+		while ((bytes=fileInputStream.read(buffer))!=-1){
+			dataOutputStream.write(buffer,0,bytes);
+			dataOutputStream.flush();
+		}
+		fileInputStream.close();
+	}
+}
