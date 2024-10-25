8f9530288b3a4b5cec4ca217e604e6149543e60f
CAZORBONNETAdrian <168072200+CAZORBONNETAdrian@users.noreply.github.com>
2024-10-25 14:25:46 +0200

    création de la fonction pour l'affichage de la notice

--- application/ControleurPadeDeGarde.java
+++ application/ControleurPadeDeGarde.java
@@ -1,8 +1,20 @@
 package application;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class ControleurPadeDeGarde {
 
@@ -36,14 +48,19 @@ public class ControleurPadeDeGarde {
     	Main.setPageImporter();
     }
 
-    @FXML
-    void notice(ActionEvent event) {
-
-    }
+//    @FXML
+//    void notice(ActionEvent event) {
+//    	 displayImages();
+//    }
 
     @FXML
     void quitter(ActionEvent event) {
     	System.exit(0);
     }
+    
+	@FXML
+	void notice(ActionEvent event) {
+		Main.afficherNotice();
+	}
 
 }
